8d703b4f0fe0de451bac914b02e0f2a692035d44
lzp <3106274101@qq.com>
2025-09-22 09:53:24 +0800

    feat: test

--- src/stores/menus.js
+++
@@ -1,9 +0,0 @@
-import { ref, computed } from 'vue'
-import { defineStore } from 'pinia'
-
-export const useMenusStore = defineStore('menus', () => {
-  const page = ref(0)
-  // const doubleCount = computed(() => count.value * 2)
-
-  return { page }
-})
